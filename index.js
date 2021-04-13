e12197825c20f8adc6b8a4a5d14586ea9df81c0c
Femke Petter <59446484+femkepetter@users.noreply.github.com>
2021-04-13 13:15:25 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use('/popper', express.static(__dirname + '/node_modules/popper.js/dist/'));
 //Vergeet niet de databasenaam newDatabase te veranderen naar een andere database
 mongoose.connect('mongodb://localhost:27017/witcherProject', {useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => {
-    console.log("MONGO CONNECTION OPEN!")
+    console.log("MONGO CONNECTION WITH INDEX.JS OPEN!")
 })
 .catch(err => {
     console.log("MONGO CONNECTION ERROR!")
@@ -31,21 +31,121 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 //***********************ROUTES*****************************/
+app.use(express.urlencoded({ extended: true }));
+
+
 app.get('/', (req, res) => {
     res.render('home', { 
         title:"Homepage",
         });
     });
 
-app.get('/quiz', async (req, res) => {
+
+app.get('/quiz/:question', async (req, res) => {
     const questions = await Question.find({})
-    const witchers = await Witcher.find({})
+    let { question } = req.params;
+    let total = 0;
+    question = parseInt(question)
+    let next_question = 0;
+
+    if (question + 1 <= 5) {
+        next_question = question + 1;
+    }
+
     res.render('quiz', { 
-        title:"Quiz", questions, witchers
+        title: "Quiz", 
+        questions, 
+        question,
+        next_question,
+        total,
     });
-    console.log(questions);
-    console.log(witchers);
 });
+
+
+app.post('/quiz/:question', async (req, res) => {
+    const questions = await Question.find({})
+    let { question } = req.params;
+    let answer = parseInt(req.body.answer);
+    let total = 0;
+    question = parseInt(question);
+
+    if (typeof req.body.result === "undefined") {
+        total = 0;
+        }
+    else {
+        total = parseInt(req.body.result);
+    };
+
+
+    console.log("Typeof Answer", typeof(answer));
+
+    console.log("Total", total);
+    console.log("Answer", answer);
+
+    let totalScore = total + answer;
+    total = totalScore;
+
+    console.log("Total score", totalScore);
+    console.log(req.body);
+
+
+    if (Number.isNaN(answer)) {
+        console.log('Number NaN');
+        } else {
+        alert("you need to type hello!");
+        return false;
+    };
+
+    let next_question = 0;
+    if (question + 1 <= 5) {
+        next_question = question + 1;
+    };
+
+    console.log("Next question", next_question);
+    console.log("Question", question);
+
+    if (question === 0) {
+        if ((totalScore >= -12) && (totalScore <= -8)) {
+        res.redirect('/result/0');
+        } else if ((totalScore >= -7) && (totalScore <= -3)) {
+        res.redirect('/result/1');
+        } else if ((totalScore >= -2) && (totalScore <= 2)) {
+        res.redirect('/result/2');
+        } else if ((totalScore >= 3) && (totalScore <= 7)) {
+        res.redirect('/result/3');
+        } else if ((totalScore >= 8) && (totalScore <= 12)) {
+        res.redirect('/result/4');
+        };
+    };
+
+    res.render('quiz', { 
+        title: "Quiz", 
+        questions,  
+        question,
+        next_question,
+        total
+    }); 
+});
+
+
+app.get('/result/:name', async (req, res) => {
+    const witchers = await Witcher.find({})
+    let { name } = req.params;
+    witcherIndex = parseInt(name)
+    let next_witcher = 0;
+     if (witcherIndex + 1 <= 5) {
+         next_witcher = name + 1;
+     }
+
+    res.render('result', { 
+        title: "Result", 
+        witchers, 
+        name,
+        next_witcher,
+        witcherIndex
+    });
+});
+
 
 app.get('/monsters', (req, res) => {
     res.render('monsters', { 

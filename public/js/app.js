e4fe6453a90ead05e86cec3561296418b991bbb6
DreamsofGanymede <65415903+DreamsofGanymede@users.noreply.github.com>
2021-04-14 09:17:18 +0200

    CSS and EJS, little bit of JS

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,21 @@
-// //***********************TEST IF JS IS LINKED****************************/
-//  alert("Hi there!");
+//***********************TEST IF JS IS LINKED****************************/
+// alert("Hi there!");
 
+$(".quiz, .monsters").click(function (e) {
+
+    $(".banner").addClass("animate__fadeOutUpBig");
+
+    $(".hone").removeClass("animate__delay-2s animate__slow");
+    $(".hone").addClass("animate__fadeOut");
+
+    $(".logo").removeClass("animate__delay-1s animate__slower");
+    $(".logo").addClass("animate__fadeOut");
+
+    $(".button").removeClass("animate__delay-3s animate__slow");
+    $(".button").addClass("animate__fadeOut");
+
+    e.preventDefault();
+    setTimeout(function (url) {
+        window.location = url
+    }, 1000, this.href);
+});

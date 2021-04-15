1b8e34c34ab338896f1d9b08fbfeb289e0d57f49
DreamsofGanymede <65415903+DreamsofGanymede@users.noreply.github.com>
2021-04-15 12:37:13 +0200

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,28 +1,35 @@
-// //***********************TEST IF JS IS LINKED****************************/
+//***********************TEST IF JS IS LINKED****************************/
 // alert("Hi there!");
+$(document).ready(function () {
+    $(".quiz, .monsters, .about").click(function (e) {
 
-$(".quiz, .monsters").click(function (e) {
+        $(".banner").addClass("animate__fadeOutUpBig");
 
-    $(".banner").addClass("animate__fadeOutUpBig");
+        $(".hone").removeClass("animate__delay-2s animate__slow");
+        $(".hone").addClass("animate__fadeOut");
 
-    $(".hone").removeClass("animate__delay-2s animate__slow");
-    $(".hone").addClass("animate__fadeOut");
+        $(".logo").removeClass("animate__delay-1s animate__slower");
+        $(".logo").addClass("animate__fadeOut");
 
-    $(".logo").removeClass("animate__delay-1s animate__slower");
-    $(".logo").addClass("animate__fadeOut");
+        $(".button").removeClass("animate__delay-3s animate__slow");
+        $(".button").addClass("animate__fadeOut");
 
-    $(".button").removeClass("animate__delay-3s animate__slow");
-    $(".button").addClass("animate__fadeOut");
+        e.preventDefault();
+        setTimeout(function (url) {
+            window.location = url
+        }, 1000, this.href);
+    });
 
-    e.preventDefault();
-    setTimeout(function (url) {
-        window.location = url
-    }, 1000, this.href);
-});
 
-$('#submit').click(function(){
-    if (!$("input[name='answer']:checked").val()) {
-       alert('Nothing is checked!');
-        return false;
-    }
-});
+    $(".quizbutton").on("click", function () {
+        $(".quizbutton").removeClass("active");
+        $(this).addClass("active");
+    })
+
+    $('#submit').click(function () {
+        if (!$("input[name='answer']:checked").val()) {
+            alert('Nothing is checked!');
+            return false;
+        }
+    })
+})
